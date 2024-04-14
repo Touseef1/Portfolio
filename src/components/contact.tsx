54666b9f6fc7c26c2efb5d98dc95d01addb1e7c0
Touseef <touseef.ahmad@otifcrew.com>
2024-04-14 14:14:15 +0500

    upload the latest changings

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -147,7 +147,7 @@ export const Contact = () => {
                 id="name"
                 value={form.name}
                 onChange={handleChange}
-                placeholder="John Doe"
+                placeholder="Touseef Ahmad"
                 title="What's your name?"
                 disabled={loading}
                 aria-disabled={loading}
@@ -169,7 +169,7 @@ export const Contact = () => {
                 id="email"
                 value={form.email}
                 onChange={handleChange}
-                placeholder="johndoe@email.com"
+                placeholder="touseefj68@gmail.com"
                 title="What's your email?"
                 disabled={loading}
                 aria-disabled={loading}
